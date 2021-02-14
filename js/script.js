19f24d1093206062a4a56f42ee58788e6765f358
vovko21 <vovako98@gmail.com>
2021-02-14 12:47:49 +0200

    Some bug fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 let img = document.getElementById("girl-img");
+let imgPhone = document.getElementById("phoneIconHeader");
 let mainForm = document.getElementById("main-form");
 mainForm.classList.add("hght-130");
 window.addEventListener('resize', HideGirlImg);
-window.addEventListener('resize', ChangeSize);
+window.addEventListener('resize', ChangeSizeMainForm);
+window.addEventListener('resize', HideImagePhone);
 
 function HideGirlImg() {
     if (window.innerWidth < 997) {
-        img.classList.add("girl-img-hidden");
+        img.classList.add("hidden");
     } else {
-        img.classList.remove("girl-img-hidden");
+        img.classList.remove("hidden");
     }
 }
 
-function ChangeSize(){
+function ChangeSizeMainForm(){
     if (window.innerWidth > 997) {
         mainForm.classList.add("hght-130");
     } else {
         mainForm.classList.remove("hght-130");
+    }
+}
+
+function HideImagePhone(){
+    if (window.innerWidth < 997) {
+        imgPhone.classList.add("hidden");
+    } else {
+        imgPhone.classList.remove("hidden");
     }
 }
