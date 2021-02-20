df517e60ff069f4fa3954ceba02791a02f45ba62
vovko21 <vovako98@gmail.com>
2021-02-20 10:35:46 +0200

    Advantages section adapted

--- js/script.js
+++ js/script.js
@@ -2,43 +2,53 @@ let img = document.getElementById("girl-img");
 let imgPhone = document.getElementById("phoneIconHeader");
 let mainForm = document.getElementById("main-form");
 let welcomeSection = document.getElementById("welcome-section");
+let advSection = document.getElementById("adv-section");
 
 window.addEventListener('resize', HideGirlImg);
 window.addEventListener('resize', ChangeSizeMainForm);
 window.addEventListener('resize', HideImagePhone);
 window.addEventListener('resize', ChangeMarginTop);
+window.addEventListener('resize', ChangeHghtAdvSection);
 
 mainForm.classList.add("hght-130");
 
 function HideGirlImg() {
-    if (window.innerWidth < 991) {
+    if (window.innerWidth < 992) {
         img.classList.add("hidden");
     } else {
         img.classList.remove("hidden");
     }
 }
 
-function ChangeSizeMainForm(){
-    if (window.innerWidth > 991) {
+function ChangeSizeMainForm() {
+    if (window.innerWidth > 992) {
         mainForm.classList.add("hght-130");
     } else {
         mainForm.classList.remove("hght-130");
     }
 }
 
-function HideImagePhone(){
-    if (window.innerWidth < 991) {
+function HideImagePhone() {
+    if (window.innerWidth < 992) {
         imgPhone.classList.add("hidden");
     } else {
         imgPhone.classList.remove("hidden");
     }
 }
 
-function ChangeMarginTop(){
-    if (window.innerWidth < 990) {
+function ChangeMarginTop() {
+    if (window.innerWidth < 992) {
         welcomeSection.classList.add("mt-200");
     } else {
         welcomeSection.classList.remove("mt-200");
+    }
+}
+
+function ChangeHghtAdvSection() {
+    if (window.innerWidth < 992) {
+        advSection.classList.add("hght-1200");
+    } else {
+        advSection.classList.remove("hght-1200");
     }
 }
 
@@ -47,4 +57,5 @@ window.onload = () => {
     ChangeSizeMainForm();
     HideImagePhone();
     ChangeMarginTop();
+    ChangeHghtAdvSection();
 }
