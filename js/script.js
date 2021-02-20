66e8e96152743f2fad892c859d290eb33b350407
vovko21 <vovako98@gmail.com>
2021-02-20 13:10:49 +0200

    Fixed/Adapted

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ let imgPhone = document.getElementById("phoneIconHeader");
 let mainForm = document.getElementById("main-form");
 let welcomeSection = document.getElementById("welcome-section");
 let advSection = document.getElementById("adv-section");
+let offImg = document.getElementById("off-img");
+let socialsImg = document.getElementById("socials-img");
+let feedbacksSection = document.getElementById("feedbacks");
 
 window.addEventListener('resize', HideGirlImg);
 window.addEventListener('resize', ChangeSizeMainForm);
 window.addEventListener('resize', HideImagePhone);
 window.addEventListener('resize', ChangeMarginTop);
 window.addEventListener('resize', ChangeHghtAdvSection);
+window.addEventListener('resize', HiddeOffImg);
+window.addEventListener('resize', HiddeSocialsImg);
+window.addEventListener('resize', FeedbacksSectionChangeSize);
 
 mainForm.classList.add("hght-130");
 
@@ -52,10 +58,38 @@ function ChangeHghtAdvSection() {
     }
 }
 
+function HiddeOffImg() {
+    if (window.innerWidth < 992) {
+        offImg.classList.add("hidden");
+    } else {
+        offImg.classList.remove("hidden");
+    }
+}
+
+function HiddeSocialsImg() {
+    if (window.innerWidth < 992) {
+        socialsImg.classList.add("hidden");
+    } else {
+        socialsImg.classList.remove("hidden");
+    }
+}
+
+function FeedbacksSectionChangeSize() {
+    if (window.innerWidth < 992) {
+        feedbacksSection.classList.add("hght-400");
+    } else {
+        feedbacksSection.classList.remove("hght-400");
+    }
+}
+
+
 window.onload = () => {
     HideGirlImg();
     ChangeSizeMainForm();
     HideImagePhone();
     ChangeMarginTop();
     ChangeHghtAdvSection();
+    HiddeOffImg();
+    HiddeSocialsImg();
+    FeedbacksSectionChangeSize();
 }
